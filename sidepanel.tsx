b7d8a177ed5140965dd44c860e0a625be8b70a99
yujhenchen <yujhen.chen93@gmail.com>
2026-05-25 21:22:00 +0800

    feat: implement side panel

---
+++ sidepanel.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import "styles/globals.css"
+
+import { Settings } from "./settings"
+
+function SidePanel() {
+  return (
+    <div className="p-4">
+      <Settings />
+    </div>
+  )
+}
+
+export default SidePanel
